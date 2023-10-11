f5d2fcd1b70369955a9a44e273c6babad54f479f
kkk2z <139834827+kkk2z@users.noreply.github.com>
2023-10-11 14:21:41 +0900

    Update slot_machine.js

--- slot_machine.js
+++ slot_machine.js
@@ -1,5 +1,5 @@
 const reels = document.getElementById('reels');
-const symbols = ['💰', '💶', '💣', '👱', '🍉', '🍓'];
+const symbols = ['💰', '💶', '💣', '👱', '🍉', '🍓','😺','☺'];
 
 function createSlotElements() {
     for (let i = 0; i < symbols.length; i++) {
